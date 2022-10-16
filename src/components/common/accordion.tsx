289b2caa8e54015647cdaf5b694ac6b7dbac13c1
David Kilson <itzkwaysi@gmail.com>
2022-10-16 13:59:23 +0100

    update accordion

--- src/components/common/accordion.tsx
+++ src/components/common/accordion.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Dropdown } from "@icons";
 
 type Props = {
 	answer: string;
@@ -18,11 +19,9 @@ export default function Accordion({ question, answer }: Props) {
 				onClick={() => show()}
 			>
 				<h4 className="flex-grow font-visby-bold text-grey md:text-[20px] text-2xl">{question}</h4>
-				{active ? (
-					<span className="cursor-pointer flex-none">-</span>
-				) : (
-					<span className="cursor-pointer flex-none">+</span>
-				)}
+				<span className={`cursor-pointer flex-none ${active ? "rotate-180" : ""}`}>
+					<Dropdown />
+				</span>
 			</div>
 			<div className={active ? "h-full block overflow-hidden accordion-content" : "h-0 hidden"}>
 				<p className="accordion-info text-black-content px-6 py-4">{answer}</p>
